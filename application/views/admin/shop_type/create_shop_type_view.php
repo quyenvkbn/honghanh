ef5a642b3a58cbfe70e40b8858966588aaa31797
minhtruongmato <truong.do@matocreative.vn>
2018-10-23 16:05:16 +0700

    [done] add shop type function in admin

---
+++ application/views/admin/shop_type/create_shop_type_view.php
@@ -0,0 +1,52 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Thêm mới
+            <small>
+                Loại Của Hàng
+            </small>
+        </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box box-default">
+                    <div class="box-body">
+                        <?php
+                        echo form_open_multipart('', array('class' => 'form-horizontal'));
+                        ?>
+                        <div class="col-xs-12">
+                            <h4 class="box-title">Thông tin cơ bản</h4>
+                        </div>
+                        <div class="row">
+                            <span><?php echo $this->session->flashdata('message'); ?></span>
+                        </div>
+                        <div class="form-group col-xs-12">
+                            <div class="form-group col-xs-12">
+                                <?php
+                                    echo form_label('Tiêu đề', 'title');
+                                    echo form_error('title');
+                                    echo form_input('title', set_value('title'), 'class="form-control" id="title"');
+                                ?>
+                            </div>
+                        </div>
+                        <div class="form-group col-xs-12">
+                            <div class="form-group col-xs-12">
+                                <?php
+                                echo form_label('Slug', 'slug');
+                                echo form_error('slug');
+                                echo form_input('slug', set_value('slug'), 'class="form-control" id="slug" readonly');
+                                ?>
+                            </div>
+                        </div>
+                        <?php echo form_submit('submit', 'OK', 'class="btn btn-primary"'); ?>
+                        <?php echo form_close(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
